ab8b1729d26d2865f0e480a78506d8e584d10526
andranik <andranik_@bk.ru>
2017-02-01 03:00:12 +0400

    hello

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AF} from "../providers/af";
+import { HomePageComponent } from './home-page/home-page.component';
+import {FormsModule} from "@angular/forms";
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyD6GT91Mg7bBQgmvcUlBeY8U4OcZfjGA8k",
+    authDomain: "fir-crud-7df6b.firebaseapp.com",
+    databaseURL: "https://fir-crud-7df6b.firebaseio.com",
+    storageBucket: "fir-crud-7df6b.appspot.com",
+    messagingSenderId: "979264159157"
+};
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegistrationPageComponent}
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes),
+    FormsModule
+  ],
+  declarations: [ AppComponent, LoginPageComponent, HomePageComponent, RegistrationPageComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [AF]
+})
+export class AppModule {}
